281366c71287eb41ca7d20d32fe3f5fd2c588249
AfonSir <afonsir17@gmail.com>
2022-03-24 19:44:24 -0300

    feat: add web project

---
+++ cmd/web/handlers.go
@@ -0,0 +1,45 @@
+package main
+
+import "net/http"
+
+func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["publishable_key"] = app.config.stripe.key
+
+	if err := app.renderTemplate(w, r, "terminal", &templateData{
+		StringMap: stringMap,
+	}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
+
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	// read posted data
+	cardholderName := r.Form.Get("cardholder_name")
+	email := r.Form.Get("cardholder_email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	data := make(map[string]interface{})
+
+	data["cardholder_name"] = cardholderName
+	data["cardholder_email"] = email
+	data["payment_intent"] = paymentIntent
+	data["payment_method"] = paymentMethod
+	data["payment_amount"] = paymentAmount
+	data["payment_currency"] = paymentCurrency
+
+	if err := app.renderTemplate(w, r, "succeeded", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
