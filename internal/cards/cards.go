c193e74b03149fda8686c409df808bc544075e4a
AfonSir <afonsir17@gmail.com>
2022-04-15 17:11:32 -0300

    feat: add payment_method and payment_intent info to receipt

--- internal/cards/cards.go
+++ internal/cards/cards.go
@@ -3,6 +3,7 @@ package cards
 import (
 	"github.com/stripe/stripe-go"
 	"github.com/stripe/stripe-go/paymentintent"
+	"github.com/stripe/stripe-go/paymentmethod"
 )
 
 type Card struct {
@@ -43,6 +44,30 @@ func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.Payment
 		return nil, msg, err
 	}
 	return pi, "", nil
+}
+
+// GetPaymentMethod gets the payment method by payment intent id
+func (c *Card) GetPaymentMethod(s string) (*stripe.PaymentMethod, error) {
+	stripe.Key = c.Secret
+
+	pm, err := paymentmethod.Get(s, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return pm, nil
+}
+
+// RetrievePaymentIntent gets an existent payment intent by id
+func (c *Card) RetrievePaymentIntent(id string) (*stripe.PaymentIntent, error) {
+	stripe.Key = c.Secret
+
+	pi, err := paymentintent.Get(id, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return pi, nil
 }
 
 func cardErrorMessage(code stripe.ErrorCode) string {
